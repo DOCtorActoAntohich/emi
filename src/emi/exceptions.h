eaa0c0a5f9f53e61774c2db1c9510a4b9b44dc29
DOCtorActoAntohich <fairy666death@mail.ru>
2022-11-06 23:00:44 +0300

    no clue

--- src/emi/exceptions.h
+++ src/emi/exceptions.h
@@ -6,12 +6,15 @@
 namespace emi {
     class window_not_found_exception : std::exception {
     public:
-        window_not_found_exception(std::string window_name) : name_(window_name) {}
+        window_not_found_exception(std::string window_name) {
+            text_ = std::format("Window not found: {}", window_name);
+        }
 
         const char* what() const noexcept override {
-            return std::format("Window not found: {}", name_).data();
+            return text_.data();
         }
+
     private:
-        std::string name_;
+        std::string text_;
     };
 }
